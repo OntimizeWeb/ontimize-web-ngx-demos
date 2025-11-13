a10424d6a1d1123984432d48a754666853438a72
Noelia Prol Carballo <noelia.prol@imatia.com>
2025-11-13 13:12:27 +0100

    Added images for cards and modified toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
 import { MatCommonModule } from '@angular/material/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+import {
+  APP_CONFIG,
+  ONTIMIZE_PROVIDERS,
+  OntimizeWebModule
+} from 'ontimize-web-ngx';
+
+import { CONFIG } from './app.config';
 
 @NgModule({
   declarations: [
@@ -15,11 +29,20 @@ import { MatCommonModule } from '@angular/material/core';
     BrowserModule,
     BrowserAnimationsModule,
     MatCommonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
     AppRoutingModule,
     HttpClientModule,
-    SharedModule
+    SharedModule,
+    OntimizeWebModule,
+    OntimizeWebModule.forRoot(CONFIG),
   ],
-  providers: [],
+  providers: [
+    { provide: APP_CONFIG, useValue: CONFIG },
+    ...ONTIMIZE_PROVIDERS
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
